9970dede441068ea4c1ed5fd3b3ce665e9e364b0
likanghua <lkh19890913@163.com>
2020-08-03 00:27:47 +0800

    feat: add sliverList  / question

--- lib/demo/sliver_demo.dart
+++ lib/demo/sliver_demo.dart
@@ -32,7 +32,8 @@ class SliverDemo extends StatelessWidget {
           SliverSafeArea(
             sliver: SliverPadding(
               padding: EdgeInsets.all(8.0),
-              sliver: SliverGridDemo()
+              // sliver: SliverGridDemo()
+              sliver: SliverListDemo(),
             ),
           )
         ],
@@ -41,16 +42,65 @@ class SliverDemo extends StatelessWidget {
   }
 }
 
-// class SliverListDemo extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return SliverList(
-//       delegate:
+class SliverListDemo extends StatelessWidget {
+  Widget _builder (BuildContext context, int index) {
+    return Padding(
+      padding: EdgeInsets.only(
+        bottom: 12.0
+      ),
+      child: Material(
+        borderRadius: BorderRadius.circular(32.0),
+        elevation: 14.0,
+        shadowColor: Colors.grey.withOpacity(0.5),
+        child: Stack(
+          children: <Widget>[
+            AspectRatio(
+              aspectRatio: 16.0/ 9.0,
+              child: Image.network(
+                posts[index].imageUrl,
+                fit: BoxFit.cover,
+              ),
+            ),
+            Positioned(
+              top: 32.0,
+              left: 32.0,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    posts[index].title,
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.white
+                    ),
+                  ),
+                  Text(
+                    posts[index].author,
+                    style: TextStyle(
+                      fontSize: 13.0,
+                      color: Colors.white
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
 
-//     );
-//   }
-// }
+  }
 
+  @override
+  Widget build(BuildContext context) {
+    return SliverList(
+      delegate:SliverChildBuilderDelegate(
+        _builder, 
+        childCount: posts.length
+      ),
+    );
+  }
+}
 
 class SliverGridDemo extends StatelessWidget {
   Widget _builder (BuildContext context, int index) {
---
+++ lib/question.dart
@@ -0,0 +1,9 @@
+
+// 1. 圆角图片怎么做  e: sliver_demo.dart
+// 2. 构造函数问题 e: layout_demo.dart
+// final IconData icon;
+// final double size;
+
+// IconBadge(this.icon, {
+//   this.size =32.0
+// });
