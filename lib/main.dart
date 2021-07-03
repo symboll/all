936515c46cb8228600e16f50c741215aa8670b20
likanghua <lkh19890913@163.com>
2021-07-04 00:13:47 +0800

    feat: init

--- lib/main.dart
+++ lib/main.dart
@@ -1,119 +1,113 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 
-import 'demo/i18n/native_localizations.dart';
-import 'demo/i18n/i18n_demo.dart';
-import 'demo/animation/animation_demo.dart';
-import 'demo/http/http_demo.dart';
-import 'demo/bloc/bloc_demo.dart';
-import 'demo/rxdart/rxdart_demo.dart';
-import 'demo/stream/stream_components.dart';
-import 'demo/state/state_components.dart';
-import 'demo/form/form_components.dart';
-import 'demo/components/material_components.dart';
-import 'demo/basic_demo.dart';
-import 'demo/bottom_navigation_bar_demo.dart';
-import 'demo/drawer_demo.dart';
-import 'demo/layout_demo.dart';
-import 'demo/list_view_demo.dart';
-import 'demo/navigator_demo.dart';
-import 'demo/sliver_demo.dart';
-import 'demo/table/table_components.dart';
-import 'demo/view_demo.dart';
-import 'demo/navigator_demo.dart' as np;
-void main() => runApp(App());
+void main() {
+  runApp(MyApp());
+}
 
-class App extends StatelessWidget {
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      localizationsDelegates: [
-        NativeLocalizationsDelegate(),
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate
-      ],
-      supportedLocales: [
-        Locale('en','US'),
-        Locale('zh','CN')
-      ],
-      debugShowCheckedModeBanner: false,
-      // home: Home(),
-      initialRoute: '/',
-      routes: {
-        '/':      (context) => NavigatorDemo(),
-        '/about': (context) => np.Page(title: 'About'),
-        '/home':  (context) =>  Home(),
-        '/form':  (context)=> FormComponents(),
-        '/mdc':   (context) => MaterialComponents(),
-        '/table': (context) => TableComponents(),
-        '/state-management': (context) => StateComponents(),
-        '/stream': (context) => StreamComponents(),
-        '/rxDart': (context) => RxDartDemo(),
-        '/bloc':   (context) => BlocDemo(),
-        '/http':   (context) => HttpDemo(),
-        '/animation': (context) => AnimationDemo(),
-        '/i18n': (context) => I18nDemo()
-      },
+      title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.yellow,
-        accentColor: Color.fromRGBO(3, 54, 255, 1.0)
-        // highlightColor: Color.fromRGBO(255, 255, 255, 0.5)
-        // splashColor: Colors.white70
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
       ),
-
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
-class Home extends StatelessWidget {
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      // This call to setState tells the Flutter framework that something has
+      // changed in this State, which causes it to rerun the build method below
+      // so that the display can reflect the updated values. If we changed
+      // _counter without calling setState(), then the build method would not be
+      // called again, and so nothing would appear to happen.
+      _counter++;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-
-    return DefaultTabController(
-      length: 4,
-      child: Scaffold(
-        backgroundColor: Colors.grey[200],
-        appBar:  AppBar(
-          // leading: IconButton(
-          //   icon: Icon(Icons.menu),
-          //   tooltip: "Navigration",
-          //   onPressed: () => {}
-          // ),
-          title: Text('Home'.toUpperCase()),
-          actions: <Widget>[
-            IconButton(icon: Icon(Icons.search), tooltip: 'Serach', onPressed: () => {
-              debugPrint('Search')
-            })
-          ],
-          elevation: 0.0,
-          bottom: TabBar(
-            unselectedLabelColor: Colors.black38,        // 未被选中的颜色
-            indicatorColor: Colors.black54 ,
-            indicatorSize: TabBarIndicatorSize.tab,      // 下面指示器的长度与 tab的长度一样
-            indicatorWeight: 1.0,
-            tabs: <Widget> [
-              Tab(icon: Icon(Icons.local_florist)),
-              Tab(icon: Icon(Icons.change_history)),
-              Tab(icon: Icon(Icons.directions_bike)),
-              Tab(icon: Icon(Icons.view_quilt))
-            ]
-          ),
-        ),
-        body: TabBarView(
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      appBar: AppBar(
+        // Here we take the value from the MyHomePage object that was created by
+        // the App.build method, and use it to set our appbar title.
+        title: Text(widget.title),
+      ),
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also a layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            ListViewDemo(),
-            BasicDemo(),
-            // LayoutDemo(),
-            ViewDemo(),
-            SliverDemo()
-          ]
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
         ),
-        drawer: DrawerDemo(),
-        bottomNavigationBar: BottomNavigationBarDemo(),
-        floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.arrow_back),
-          onPressed: ()=> Navigator.pop(context)
-        ),
-      )
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
